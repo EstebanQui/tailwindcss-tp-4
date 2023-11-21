df6c4053ac006a654f1c8fc6c99f29cde2f9003c
Esteban Quinones <esteban.quinones@outlook.fr>
2023-11-21 16:33:01 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bgColor' : "#F7F7F7",
+      },
+    },
+  },
+  plugins: [],
+}
